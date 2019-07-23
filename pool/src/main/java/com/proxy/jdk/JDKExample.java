be6bbc865f435567de1cce2da56bb9c4b2cc516a
戎志宏 <rongzhihong@lixin360.com>
2019-07-23 11:23:36 +0800

    Add 测试JDK代理无接口时是否异常

--- pool/src/main/java/com/proxy/jdk/JDKExample.java
+++ pool/src/main/java/com/proxy/jdk/JDKExample.java
@@ -9,5 +9,12 @@ public class JDKExample {
         JdkProxy jdkProxy = new JdkProxy(helloInterface);
         HelloInterface helloProxy = (HelloInterface) Proxy.newProxyInstance(jdkProxy.getClass().getClassLoader(), helloInterface.getClass().getInterfaces(), jdkProxy);
         helloProxy.sayHello();
+
+        // 测试不使用接口是否报错
+/*        HelloInterfaceImpl helloInterface2 = new HelloInterfaceImpl();
+        JdkProxy jdkProxy2 = new JdkProxy(helloInterface2);
+        HelloInterfaceImpl helloProxy2 = (HelloInterfaceImpl) Proxy.newProxyInstance(jdkProxy2.getClass().getClassLoader(), helloInterface2.getClass().getInterfaces(), jdkProxy2);
+        helloProxy2.sayHello();*/
+        // 结果报错：java.lang.ClassCastException: com.sun.proxy.$Proxy0 cannot be cast to com.proxy.jdk.HelloInterfaceImpl
     }
 }
